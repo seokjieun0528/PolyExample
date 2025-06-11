73c785ac97528632e57a0a84bc1de07486002b19
Another Name <s2307@e-mirim.hs.kr>
2025-06-11 13:59:43 +0900

    강아지 고양이 클래스 예제

---
+++ Dog.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PolyExample
+{
+    class Dog
+    {
+        public Dog()
+        {
+            Age = 0;
+        }
+
+        public int Age { get; set; }
+        public string Color { get; set; }
+
+        public void Eat() { Console.WriteLine("냠냠 먹습니다."); }
+        public void Sleep() { Console.WriteLine("쿨쿨 잠을 잡니다."); }
+        public void Bark() { Console.WriteLine("왈왈 짓습니다."); }
+    }
+}
